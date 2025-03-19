029f9024d50dd0ce585782914dd1ab0d7e9139f3
simone002 <simonebattiato002@gmail.com>
2025-03-19 10:08:55 +0100

    upgrade

--- models/sale_order.py
+++ models/sale_order.py
@@ -5,10 +5,11 @@ class SaleOrder(models.Model):
 
     client_order_ref = fields.Char(string="Client Order Reference", copy=False, readonly=True)
     
-    @api.model
-    def create(self, vals):
-        record = super(SaleOrder, self).create(vals)
-        # Check if this is a subscription order (has visible plan_id)
-        if record.plan_id and record.plan_id.visible:
-            record.client_order_ref = self.env['ir.sequence'].next_by_code('sale.order.subscription.ref') or '/'
-        return record
+    @api.model_create_multi
+    def _create(self, vals_list):
+        records = super(SaleOrder, self)._create(vals_list)
+        for record in records:
+            # Check if this is a subscription order (has visible plan_id)
+            if record.plan_id and record.plan_id.visible:
+                record.client_order_ref = self.env['ir.sequence'].next_by_code('sale.order.subscription.ref') or '/'
+        return records

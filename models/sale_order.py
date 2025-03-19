0ac8eb05518f42d73a9cd8127196487fb5c7113d
simone002 <simonebattiato002@gmail.com>
2025-03-19 10:04:33 +0100

    upgrade

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,8 +3,12 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    client_order_ref = fields.Char(string="Client Order Reference", copy=False, readonly=True, default=lambda self: self._generate_client_order_ref())
-
+    client_order_ref = fields.Char(string="Client Order Reference", copy=False, readonly=True)
+    
     @api.model
-    def _generate_client_order_ref(self):
-        return self.env['ir.sequence'].next_by_code('sale.order.subscription.ref') or '/'
+    def create(self, vals):
+        record = super(SaleOrder, self).create(vals)
+        # Check if this is a subscription order (has visible plan_id)
+        if record.plan_id and record.plan_id.visible:
+            record.client_order_ref = self.env['ir.sequence'].next_by_code('sale.order.subscription.ref') or '/'
+        return record
